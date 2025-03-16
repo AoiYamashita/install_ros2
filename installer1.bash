2d8d3337f2da2abfe5e5b9577a17bf5d28273571
AoiYamashita <154947689+AoiYamashita@users.noreply.github.com>
2025-03-16 23:20:51 +0900

    Update installer1.bash

--- installer1.bash
+++ installer1.bash
@@ -2,6 +2,8 @@
 
 #installwe ubuntu 22.04
 
+cd ~/
+
 sudo apt update && sudo apt install curl gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
